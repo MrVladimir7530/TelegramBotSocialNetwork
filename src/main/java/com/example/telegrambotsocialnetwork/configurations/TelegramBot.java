dbd3880b6fbf3e1e396e25dd5fdd59efedc3f053
MrVladimir <vladimir.volkov-2013@yandex.ru>
2023-11-21 11:20:18 +0300

    Test the bot for connection

--- src/main/java/com/example/telegrambotsocialnetwork/configurations/TelegramBot.java
+++ src/main/java/com/example/telegrambotsocialnetwork/configurations/TelegramBot.java
@@ -2,22 +2,39 @@ package com.example.telegrambotsocialnetwork.configurations;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
 @Slf4j
 @RequiredArgsConstructor
+@Service
 public class TelegramBot extends TelegramLongPollingBot {
     private final BotConfig botConfig;
 
     @Override
     public void onUpdateReceived(Update update) {
-
+        if (update.getMessage().getText().equals("Привет")) {
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setChatId(update.getMessage().getChatId());
+            sendMessage.setText("Привет Владимир");
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     @Override
     public String getBotUsername() {
         return botConfig.getBotName();
+    }
+    @Override
+    public String getBotToken() {
+        return botConfig.getToken();
     }
 }
