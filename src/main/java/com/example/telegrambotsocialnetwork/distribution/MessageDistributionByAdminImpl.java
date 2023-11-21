a4048ce4e17b0918fa805f2f96e43546c9ebd60a
MrVladimir <vladimir.volkov-2013@yandex.ru>
2023-11-21 21:27:18 +0300

    Added name in message

--- src/main/java/com/example/telegrambotsocialnetwork/distribution/MessageDistributionByAdminImpl.java
+++ src/main/java/com/example/telegrambotsocialnetwork/distribution/MessageDistributionByAdminImpl.java
@@ -1,19 +1,21 @@
 package com.example.telegrambotsocialnetwork.distribution;
 
 import com.example.telegrambotsocialnetwork.configurations.SendBotMessage;
+import com.example.telegrambotsocialnetwork.model.User;
 import com.example.telegrambotsocialnetwork.repository.AdminRepository;
+import com.example.telegrambotsocialnetwork.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MessageDistributionByAdminImpl implements MessageDistributionByAdmin{
     private final SendBotMessage sendBotMessage;
     private final AdminRepository adminRepository;
+    private final UsersRepository usersRepository;
 
     @Override
     public SendMessage distribution(Update update) {
@@ -23,8 +25,18 @@ public class MessageDistributionByAdminImpl implements MessageDistributionByAdmi
             Long chatId = update.getMessage().getChatId();
             Long companionId = adminRepository.findCompanionIdByChatId(chatId);
             String textMessage = update.getMessage().getText();
+            textMessage = getUserNameOrFirstName(companionId) + ": " + textMessage;
             sendBotMessage.sendMessage(companionId, textMessage);
         }
         return null;
+    }
+
+    private String getUserNameOrFirstName(Long companionId) {
+        User user = usersRepository.findByChatId(companionId);
+        String userName = user.getUserName();
+        if (userName != null) {
+            return userName;
+        }
+        return user.getFirstName();
     }
 }
